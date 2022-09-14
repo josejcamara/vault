97ff19859c8d8ae52a7b3afc500750c59002b28a
Jose Jimenez <Jose.Jimenez@nanoporetech.com>
2022-09-14 19:02:04 +0100

    more auth commands

--- authentication.sh
+++ authentication.sh
@@ -1,25 +1,91 @@
 #!/bin/bash
 
-# =====================
-# Enable Auth Methods 
-# =====================
-
-# Now set your Vault address environment variable
-export VAULT_ADDR=http://127.0.0.1:8200
+# Run vault-dev
+vault server -dev
 
 # Log into Vault (use the root token)
+export VAULT_ADDR=http://127.0.0.1:8200
 vault login 
 
-# See auth methods avilable
+
+# ============================
+# List Auth Methods enabled
+# ============================
 vault auth list
+
+
+# ============================
+# Help Auth Methods
+# ============================
+vault path-help auth/userpass/
+vault path-help auth/userpass/users
+vault path-help auth/userpass/users/something
+
+
+# ============================
+# Enable Auth Methods 
+# ============================
+# > vault auth enable [options] TYPE
+# Multiple instances of the same method can be enabled on different paths (default path = method's name)
 
 # Enable first auth method using userpass
 vault auth enable userpass
+vault auth list
+vault auth enable -path=group1_pass userpass
 
 # Enable AppRole as well (can try using the UI)
 vault auth enable -description="Demo AppRole" approle
 vault auth list
 
-# Fix description
+
+# ============================
+# Tune/Configure Auth Methods 
+# ============================
+# > vault auth tune [options] PATH
+
+# Tune description
 vault auth tune -description="Demo Userpass" userpass/
+vault auth list
+
+# Create a new user
+vault write auth/userpass/users/jose password=jose
+
+# Create a role
+vault write auth/approle/role/cicd role_name="integration" secret_id_num_uses=1 secret_id_tll=1h
+
+
+# ============================
+# Use Auth Methods 
+# ============================
+
+# Login using token
+vault login
+
+# Login with an userpass
+# > vault login [options] AUTH -OR- vault write [options] PATH DATA
+vault login -method=userpass username=jose
+# > it prompts for the password
+# OR
+vault write auth/userpass/login/jose password=jose
+
+# Login with approle
+# > vault path-help auth/approle/login
+roleId <= vault read auth/approle/role/integration/role-id
+secretId <= vault write -force auth/approle/role/integration/secret-id
+
+vault write auth/approle/login role_id=$roleId secret_id=$secretId
+
+# Same login but with the API
+curl --request POST \
+    --data "{\"role_id\": \"$roleId\", \"secret_id\": \"$secretId\"}" \
+    $VAULT_ADDR/v1/auth/approle/login | jq
+
+
+# ============================
+# Disable Auth Methods 
+# ============================
+# > vault auth disable [options] PATH
+
+# ** WARNING **: Disabling a method also deletes all the information stored about that method
+vault auth disable userpass
 vault auth list
